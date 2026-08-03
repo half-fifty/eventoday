7643d662f23f729a753544b67d0ffbe09022b4be
suho <qusql98@naver.com>
2026-08-03 17:46:37 +0900

    Update BoothInterestService.java

--- BE/src/main/java/com/min/edu/interest/service/BoothInterestService.java
+++ BE/src/main/java/com/min/edu/interest/service/BoothInterestService.java
@@ -2,11 +2,10 @@ package com.min.edu.interest.service;
 
 import com.min.edu.interest.dto.InterestBoothResponse;
 import com.min.edu.interest.repository.BoothInterestRepository;
-import com.min.edu.booth.domain.BoothInterest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.time.OffsetDateTime;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,18 +13,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Transactional
 public class BoothInterestService {
+
     private final BoothInterestRepository boothInterestRepository;
 
     public void register(Long memberId, Long boothId) {
-        if (boothInterestRepository.existsByMemberIdAndBoothId(memberId, boothId)) {
-            return;
-        }
-        boothInterestRepository.save(BoothInterest.builder()
-                .memberId(memberId)
-                .boothId(boothId)
-                .vacancyNotificationEnabled(false)
-                .createdAt(OffsetDateTime.now())
-                .build());
+        boothInterestRepository.upsertInterest(memberId, boothId);
     }
 
     public void remove(Long memberId, Long boothId) {
